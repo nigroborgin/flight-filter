92d5ab62b772119c1d95a9f09f4ed82df6e91540
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2021-10-28 12:27:18 +0300

    made more readable

--- src/main/java/com/gridnine/testing/DepartureBeforeNow.java
+++ src/main/java/com/gridnine/testing/DepartureBeforeNow.java
@@ -7,7 +7,11 @@ public class DepartureBeforeNow implements FlightFilter {
     @Override
     public boolean check(Flight flight) {
         for (Segment segment : flight.getSegments()) {
-            if (segment.getDepartureDate().isBefore(LocalDateTime.now())) {
+
+            LocalDateTime departure = segment.getDepartureDate();
+            LocalDateTime now = LocalDateTime.now();
+
+            if (departure.isBefore(now)) {
                 return true;
             }
         }
