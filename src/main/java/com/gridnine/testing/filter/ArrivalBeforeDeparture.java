9574819e7da20592f1f65b000197045adc8d7a18
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2021-10-28 16:25:45 +0300

    small refactoring code

--- src/main/java/com/gridnine/testing/filter/ArrivalBeforeDeparture.java
+++ src/main/java/com/gridnine/testing/filter/ArrivalBeforeDeparture.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class ArrivalBeforeDeparture implements FlightFilter {
+
     @Override
     public boolean check(Flight flight) {
         List<Segment> segments = flight.getSegments();
