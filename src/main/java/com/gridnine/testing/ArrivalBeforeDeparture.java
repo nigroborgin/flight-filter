08dfb842634372b4f6a48300095cb0651cd72fcc
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2021-10-28 12:33:18 +0300

    List of segments is placed in a separate variable

--- src/main/java/com/gridnine/testing/ArrivalBeforeDeparture.java
+++ src/main/java/com/gridnine/testing/ArrivalBeforeDeparture.java
@@ -1,22 +1,25 @@
 package com.gridnine.testing;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ArrivalBeforeDeparture implements FlightFilter {
     @Override
     public boolean check(Flight flight) {
-        if (flight.getSegments().size() == 1) {
+        List<Segment> segments = flight.getSegments();
 
-            LocalDateTime departure = flight.getSegments().get(0).getDepartureDate();
-            LocalDateTime arrival = flight.getSegments().get(0).getArrivalDate();
+        if (segments.size() == 1) {
+
+            LocalDateTime departure = segments.get(0).getDepartureDate();
+            LocalDateTime arrival = segments.get(0).getArrivalDate();
             return arrival.isBefore(departure);
 
         } else {
-            for (int i = 0; i < flight.getSegments().size() - 1; i++) {
+            for (int i = 0; i < segments.size() - 1; i++) {
 
-                LocalDateTime departure = flight.getSegments().get(i).getDepartureDate();
-                LocalDateTime arrival = flight.getSegments().get(i).getArrivalDate();
-                LocalDateTime departureNextSegment = flight.getSegments().get(i+1).getDepartureDate();
+                LocalDateTime departure = segments.get(i).getDepartureDate();
+                LocalDateTime arrival = segments.get(i).getArrivalDate();
+                LocalDateTime departureNextSegment = segments.get(i+1).getDepartureDate();
 
                 if (arrival.isBefore(departure) || departureNextSegment.isBefore(arrival)) {
                     return true;
