ef631243ff2006cb566957fa3df0ec4a97b1d027
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2021-10-29 12:50:21 +0300

    made contructors are public

--- src/main/java/com/gridnine/testing/flights/Flight.java
+++ src/main/java/com/gridnine/testing/flights/Flight.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class Flight {
     private final List<Segment> segments;
 
-    Flight(final List<Segment> segs) {
+    public Flight(final List<Segment> segs) {
         segments = segs;
     }
 
