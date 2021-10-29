7b863b9f1d8ef06140d6738102206508071e4486
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2021-10-29 10:09:39 +0300

    generate Test classes for filters

---
+++ src/test/java/com/gridnine/testing/filter/MoreTwoHoursOnGroundTest.java
@@ -0,0 +1,12 @@
+package com.gridnine.testing.filter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoreTwoHoursOnGroundTest {
+
+    @Test
+    void check() {
+    }
+}
