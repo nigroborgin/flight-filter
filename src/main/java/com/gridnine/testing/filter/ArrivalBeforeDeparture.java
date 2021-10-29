72cc8cae145411e8b068e4f36cadd207281a48d4
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2021-10-29 12:49:26 +0300

    add check "null" or empty segments list

--- src/main/java/com/gridnine/testing/filter/ArrivalBeforeDeparture.java
+++ src/main/java/com/gridnine/testing/filter/ArrivalBeforeDeparture.java
@@ -6,13 +6,19 @@ import com.gridnine.testing.flights.Segment;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class ArrivalBeforeDeparture implements FlightFilter {
+public class ArrivalBeforeDeparture implements FlightFilter{
 
     @Override
-    public boolean check(Flight flight) {
-        List<Segment> segments = flight.getSegments();
+    public boolean check(Flight flight) throws IllegalArgumentException{
+        if (flight == null) {
+            throw new NullPointerException("This flight is null");
+        }
 
-        if (segments.size() == 1) {
+        List<Segment> segments = flight.getSegments();
+        if (segments.size() == 0) {
+            throw new IllegalArgumentException("This flight does not contain segments");
+
+        } else if (segments.size() == 1) {
 
             LocalDateTime departure = segments.get(0).getDepartureDate();
             LocalDateTime arrival = segments.get(0).getArrivalDate();
@@ -23,7 +29,7 @@ public class ArrivalBeforeDeparture implements FlightFilter {
 
                 LocalDateTime departure = segments.get(i).getDepartureDate();
                 LocalDateTime arrival = segments.get(i).getArrivalDate();
-                LocalDateTime departureNextSegment = segments.get(i+1).getDepartureDate();
+                LocalDateTime departureNextSegment = segments.get(i + 1).getDepartureDate();
 
                 if (arrival.isBefore(departure) || departureNextSegment.isBefore(arrival)) {
                     return true;
